d829a9610ca1f756f0acbc26c3f68bf6a18095df
Nggohel <gohelnidhi15898@gmail.com>
2023-12-25 21:24:58 +0530

    add Factorial Logic

--- components/FactorialComponent.js
+++ components/FactorialComponent.js
@@ -1,13 +1,16 @@
-
+import { useMemo } from "react";
 
 function factorial(n) {
   if (n <= 1) return 1;
   return n * factorial(n - 1);
 }
 
-export default function FactorialComponent() {
+export default function FactorialComponent({ number }) {
+  const result = useMemo(() => factorial(number), [number]);
 
-
-  return <div className='result'>The factorial of {} is {}</div>;
+  return (
+    <div className="result">
+      The factorial of {number} is {result}
+    </div>
+  );
 }
-
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
-"use client"
-import { useState } from 'react';
-import FactorialComponent from '../components/FactorialComponent';
+import { useState } from "react";
+import FactorialComponent from "../components/FactorialComponent";
+
 export default function Home() {
-  
-     
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    const value = parseInt(e.target.value, 10) || 0; // Ensure a valid integer value
+    setInputValue(value);
+  };
+
   return (
     <div>
-        <h1>Welcome to Next.js!</h1>
-        <section>
-                <h2>Factorial Calculation</h2>
-                <input 
-                    type="number" 
-                    
-                   } 
-                />
-       
-                <FactorialComponent  />
-            </section>
+      <h1>Welcome to Next.js!</h1>
+      <section>
+        <h2>Factorial Calculation</h2>
+        <input type="number" value={inputValue} onChange={handleInputChange} />
+        <FactorialComponent number={inputValue} />
+      </section>
     </div>
-);
+  );
 }
